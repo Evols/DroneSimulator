5ab35c4583b682f18cbc9827ef30fec3934c680f
Baptiste Hutteau <hutteau.b@gmail.com>
2025-12-08 00:02:23 +0100

    feat: in simplified propulsion model, taking the speed into account to compute force

--- Source/DroneSimulator/PropulsionModel/PropulsionModelDirectSetpoint.cpp
+++ Source/DroneSimulator/PropulsionModel/PropulsionModelDirectSetpoint.cpp
@@ -10,7 +10,11 @@ TOptional<FDynamicsPropellerSetInfo> UPropulsionModelDirectSetpoint::tick_propul
     substep_body->angular_velocity_radians_world = substep_body->transform_world.TransformVectorNoScale(drone_setpoint.angular_velocity_radians);
 
     const auto drone_up_axis = substep_body->transform_world.GetUnitAxis(EAxis::Z);
-    const auto force_to_apply = drone_up_axis * drone_setpoint.throttle * this->max_throttle_force;
+
+    const auto drone_vertical_velocity = FVector::DotProduct(drone_up_axis, substep_body->linear_velocity_world);
+    const auto max_speed_factor = FMath::Max(0.0, 1.0 - drone_vertical_velocity / this->max_vertical_speed);
+
+    const auto force_to_apply = drone_up_axis * drone_setpoint.throttle * max_speed_factor * this->max_throttle_force;
     substep_body->add_force(force_to_apply);
 
     return {};
--- Source/DroneSimulator/PropulsionModel/PropulsionModelDirectSetpoint.h
+++ Source/DroneSimulator/PropulsionModel/PropulsionModelDirectSetpoint.h
@@ -13,7 +13,10 @@ class UPropulsionModelDirectSetpoint : public UPropulsionModel
 public:
 
     UPROPERTY(EditAnywhere)
-    double max_throttle_force = 20.0;
+    double max_throttle_force = 50.0;
+
+    UPROPERTY(EditAnywhere, meta=(DisplayName="Max vertical speed (m/s)"))
+    double max_vertical_speed = 30.0;
 
     virtual TOptional<FDynamicsPropellerSetInfo> tick_propulsion(double delta_time, FSubstepBody* substep_body,
         const FDroneSetpoint& drone_setpoint, const FPropulsionDroneSetup& drone_setup,
